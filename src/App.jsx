7ec75b4dba7921569d74f735a26a3d960b379e47
ibrahimzyusuf <ibramhashalyusuf880@gmail.com>
2025-02-23 16:30:24 +0300

    adding BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import {Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Packages from "./Pages/Packages/Packages";
@@ -8,13 +8,13 @@ import Packages from "./Pages/Packages/Packages";
 function App() {
   
   return (
-    <>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/packages' element={<Packages/>} />
-    </Routes>
-    </>
+    <BrowserRouter basename="/travel-app-frontend">
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/packages' element={<Packages/>} />
+      </Routes>
+    </BrowserRouter>  
   )
 }
 
