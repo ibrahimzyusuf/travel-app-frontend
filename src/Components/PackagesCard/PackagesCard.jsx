bc1e22bd23df37f0f3688a635b703d189af0e90a
ibrahimzyusuf <ibramhashalyusuf880@gmail.com>
2025-10-01 07:50:29 +0300

    trendy card fix1

--- src/Components/PackagesCard/PackagesCard.jsx
+++ src/Components/PackagesCard/PackagesCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PackagesCard = ({card_img,card_date,card_visitors,card_title,card_desc,card_price,card_rate}) => {
   return (
-    <div className='packages_card w-full md:w-96 p-5 flex flex-col gap-5 bg-gray3'>
+    <div className='packages_card w-full md:w-96 p-5 flex flex-col gap-4 bg-gray3'>
         <div className="top border-0 rounded">
             <img src={card_img} alt="packages card image" className='w-full' loading='lazy' />
             <div className="info w-full h-10 rounded-b bg-red-500 flex items-center px-4 justify-between font-Pop font-medium text-xs text-white">
